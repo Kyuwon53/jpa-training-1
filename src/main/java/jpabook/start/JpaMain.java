bb62d760b361442d1fe346220278722bf4f7bf8e
kyuwon53 <lkw2v@naver.com>
2022-03-14 14:41:59 +0900

    [추가] jpa 관련 설정 추가

--- src/main/java/jpabook/start/JpaMain.java
+++ src/main/java/jpabook/start/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -25,6 +26,23 @@ public class JpaMain {
     }
 
     private static void logic(EntityManager em) {
+        String id = "id";
+        Member member = new Member();
+        member.setId(id);
+        member.setUsername("규원");
+        member.setAge(28);
 
+        em.persist(member);
+
+        member.setAge(27);
+
+        Member findMember = em.find(Member.class, id);
+        System.out.println("findMember= " + findMember.getUsername() + ", age= " + findMember.getAge());
+
+        List<Member> members = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+        System.out.println("members.size= " + members.size());
+
+        em.remove(member);
     }
 }
