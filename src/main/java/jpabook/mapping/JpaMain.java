cb1b2765954625dc252971378b60ce3731893a89
kyuwon53 <lkw2v@naver.com>
2022-03-17 15:48:52 +0900

    [추가] 엔티티 저장 메소드 추가

--- src/main/java/jpabook/mapping/JpaMain.java
+++ src/main/java/jpabook/mapping/JpaMain.java
@@ -1,15 +1,39 @@
 package jpabook.mapping;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class JpaMain {
     public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            testSave(em);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+    private static void testSave(EntityManager em){
+        Team team1 = new Team("team1", "팀1");
+        em.persist(team1);
 
         Member member1 = new Member("member1", "회원1");
-        Member member2 = new Member("member2", "회원2");
-        Team team1 = new Team("team1", "팀1");
-
         member1.setTeam(team1);
-        member2.setTeam(team1);
+        em.persist(member1);
 
-        Team findTeam = member1.getTeam();
+        Member member2 = new Member("member2", "회원2");
+        member1.setTeam(team1);
+        em.persist(member2);
+
     }
 }
