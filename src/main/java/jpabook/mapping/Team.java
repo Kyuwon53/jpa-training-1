52346f7e525376d806d2c8b2c8dc8fcea1775db5
kyuwon53 <lkw2v@naver.com>
2022-03-17 15:28:01 +0900

    [설정] 객체 관계 매핑 추가 팀 엔티티 매핑

--- src/main/java/jpabook/mapping/Team.java
+++ src/main/java/jpabook/mapping/Team.java
@@ -1,7 +1,13 @@
 package jpabook.mapping;
 
-public class Team {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
+public class Team {
+    @Id
+    @Column(name = "TEAM_ID")
     private String id;
     private String name;
 
