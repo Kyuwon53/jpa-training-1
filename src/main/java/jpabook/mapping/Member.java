c409d32a4d71f931f13cd0d3b46e70f521f64a67
kyuwon53 <lkw2v@naver.com>
2022-03-17 15:27:16 +0900

    [추가] 객체 관계 매핑 설정 team에 연관관계 매핑 설정 다대일

--- src/main/java/jpabook/mapping/Member.java
+++ src/main/java/jpabook/mapping/Member.java
@@ -1,9 +1,21 @@
 package jpabook.mapping;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Member {
+    @Id
+    @Column(name = "MEMBER_ID")
     private String id;
+
     private String username;
 
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
     private Team team;
 
     public Member(String id, String username) {
