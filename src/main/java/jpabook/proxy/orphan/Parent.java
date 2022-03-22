0ac2e5696fe68cf76937e5f73906081928734c6f
kyuwon53 <lkw2v@naver.com>
2022-03-22 22:09:44 +0900

    [생성] 고아 객체 - 부모 객체 생성 참조가 제거된 인티티는 고아 객체로 보고 삭제된다.

---
+++ src/main/java/jpabook/proxy/orphan/Parent.java
@@ -0,0 +1,19 @@
+package jpabook.proxy.orphan;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Parent {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @OneToMany(mappedBy = "parent", orphanRemoval = true)
+    private List<Child> children = new ArrayList<Child>();
+}
