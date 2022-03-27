d4dc55a3974e8628035c2e57dffd492aabb81d3c
kyuwon53 <lkw2v@naver.com>
2022-03-27 17:16:05 +0900

    remove setter immutable Object

--- src/main/java/jpabook/valuetype/Address.java
+++ src/main/java/jpabook/valuetype/Address.java
@@ -16,35 +16,26 @@ public class Address {
     public Address() {
     }
 
-    public String getState() {
-        return state;
+    public Address(String city, String street, String state, Zipcode zipcode) {
+        this.city = city;
+        this.street = street;
+        this.state = state;
+        this.zipcode = zipcode;
     }
 
-    public void setState(String state) {
-        this.state = state;
+    public String getState() {
+        return state;
     }
 
     public String getCity() {
         return city;
     }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
-
     public String getStreet() {
         return street;
     }
 
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
     public Zipcode getZipcode() {
         return zipcode;
-    }
-
-    public void setZipcode(Zipcode zipcode) {
-        this.zipcode = zipcode;
     }
 }
